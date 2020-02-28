3c79c57139d4586fd74e79b5a43df60bac5056d8
FHHF <fabianhumbertohernandez@gmail.com>
2020-02-28 11:57:56 +0200

    department repo

---
+++ src/main/java/org/dev/fhhf/testtask/repo/DepartmentDao.java
@@ -0,0 +1,18 @@
+package org.dev.fhhf.testtask.repo;
+
+import org.dev.fhhf.testtask.model.Department;
+
+import java.util.List;
+
+public interface DepartmentDao {
+
+    List<Department> findAllDepartments();
+
+    Department findDepartmentById(int dpId);
+
+    Department insertDepartment(Department department);
+
+    Department updateDepartment(Department department);
+
+    void deleteDepartment(Department department);
+}
---
+++ src/main/java/org/dev/fhhf/testtask/repo/DepartmentDaoImpl.java
@@ -0,0 +1,51 @@
+package org.dev.fhhf.testtask.repo;
+
+import org.dev.fhhf.testtask.model.Department;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class DepartmentDaoImpl implements DepartmentDao {
+
+    @PersistenceContext(unitName="JPASpring")
+    private EntityManager em;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Department> findAllDepartments() {
+        return em.createNamedQuery("Department.findAll")
+                .getResultList();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Department findDepartmentById(int dpId) {
+        return (Department) em.createNamedQuery("Department.findById")
+                .setParameter("dpId", dpId)
+                .getSingleResult();
+    }
+
+    @Override
+    @Transactional
+    public Department insertDepartment(Department department) {
+        em.persist(department);
+        return department;
+    }
+
+    @Override
+    @Transactional
+    public Department updateDepartment(Department department) {
+        em.merge(department);
+        return department;
+    }
+
+    @Override
+    @Transactional
+    public void deleteDepartment(Department department) {
+        em.remove( em.merge(department) );
+    }
+}
