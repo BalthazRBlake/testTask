d524fcf5ceca7b98c8b4b0d4c68ac583114e9ed5
FHHF <fabianhumbertohernandez@gmail.com>
2020-02-28 11:45:36 +0200

    entity ready

--- src/main/java/org/dev/fhhf/testtask/model/Employee.java
+++ src/main/java/org/dev/fhhf/testtask/model/Employee.java
@@ -1,33 +1,50 @@
 package org.dev.fhhf.testtask.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
+@NamedQueries({
+        @NamedQuery(name = "Employees.findAll", query = "SELECT e FROM Employee e"),
+        @NamedQuery(name = "Employees.nameStartsWith", query = "SELECT e FROM Employee e WHERE e.empName LIKE :nameStartsWith"),
+        @NamedQuery(name = "Employee.findById", query = "SELECT e FROM Employee e WHERE e.empId = :empId")
+})
 @Table(name = "tblemployees")
+@ApiModel(description = "Employee details")
 public class Employee implements Serializable {
 
     private static final long serialVersionUD = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Basic(optional = false)
     @Column(name = "empID", nullable = false, updatable = false)
     private Integer empId;
 
     @Column(name = "empName")
+    @ApiModelProperty(required = true, example = "Pete")
     private String empName;
 
     @Column(name = "empActive")
+    @ApiModelProperty(required = true)
     private Boolean empActive;
 
-    //private Integer emp_dpID; //Foreing KEY
+    @OneToOne
+    @JoinColumn(name = "emp_dpID")
+    private Department department;
 
     public Employee() {
     }
 
     public Employee(Integer empId) {
         this.empId = empId;
+    }
+
+    public Employee(String empName, Boolean empActive) {
+        this.empName = empName;
+        this.empActive = empActive;
     }
 
     public Integer getEmpId() {
@@ -52,5 +69,23 @@ public class Employee implements Serializable {
 
     public void setEmpActive(Boolean empActive) {
         this.empActive = empActive;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empId=" + empId +
+                ", empName='" + empName + '\'' +
+                ", empActive=" + empActive +
+                ", department=" + department +
+                '}';
     }
 }
