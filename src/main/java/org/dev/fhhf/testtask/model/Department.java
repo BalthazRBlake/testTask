3596b44b448ebf3fc41bbc3e7ebfcd93b820914e
FHHF <fabianhumbertohernandez@gmail.com>
2020-02-28 11:48:53 +0200

    department ready

--- src/main/java/org/dev/fhhf/testtask/model/Department.java
+++ src/main/java/org/dev/fhhf/testtask/model/Department.java
@@ -1,11 +1,19 @@
 package org.dev.fhhf.testtask.model;
 
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
+@NamedQueries({
+        @NamedQuery(name = "Department.findAll", query = "SELECT d FROM Department d"),
+        @NamedQuery(name = "Department.findById", query = "SELECT d FROM Department d WHERE d.dpId = :dpId")
+})
 @Table(name = "tbldepartments")
 public class Department implements Serializable {
+
+    private static final long serialVersionUD = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,6 +21,7 @@ public class Department implements Serializable {
     private Integer dpId;
 
     @Column(name = "dpName")
+    @ApiModelProperty(required = true)
     private String dpName;
 
     public Department() {
@@ -22,8 +31,7 @@ public class Department implements Serializable {
         this.dpId = dpId;
     }
 
-    public Department(Integer dpId, String dpName) {
-        this.dpId = dpId;
+    public Department(String dpName) {
         this.dpName = dpName;
     }
 
@@ -42,4 +50,13 @@ public class Department implements Serializable {
     public void setDpName(String dpName) {
         this.dpName = dpName;
     }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "dpId=" + dpId +
+                ", dpName='" + dpName + '\'' +
+                '}';
+    }
+
 }
